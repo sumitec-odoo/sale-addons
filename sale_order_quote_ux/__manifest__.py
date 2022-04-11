b9a71ceec22fda6c7dae9607519f53819077bd48
Juan Pablo Garza <juanp@juanpgarza.com>
2022-04-11 17:46:09 -0300

    [15.0][FIX] sale_order_quote_ux: le quito el grupo de seguridad para que los usuarios públicos puedan ver el pie

--- sale_order_quote_ux/__manifest__.py
+++ sale_order_quote_ux/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "sale_order_quote_price",
+    "name": "sale_order_quote_ux",
     "summary": "",
     "version": "15.0.1.0.0",
     "category": "Sales",
