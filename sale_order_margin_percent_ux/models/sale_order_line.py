a507c53f6477cafbda3a4873d8bd5b2ed7b7ae41
Juan Pablo Garza <juanp@juanpgarza.com>
2023-05-08 12:35:54 -0300

    _compute_margin lo hago SOLO en el módulo en clima

--- sale_order_margin_percent_ux/models/sale_order_line.py
+++ sale_order_margin_percent_ux/models/sale_order_line.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api, _
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+    # _compute_margin lo hago SOLO en el módulo en clima
     @api.depends('price_subtotal', 'product_uom_qty', 'purchase_price')
     def _compute_margin(self):
         super(SaleOrderLine,self)._compute_margin()
