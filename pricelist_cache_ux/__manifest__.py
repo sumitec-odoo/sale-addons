25e4ba51b0024010fabdfe6ab5bb0f3da296c4dc
Juan Pablo Garza <juanp@juanpgarza.com>
2023-07-13 09:09:12 -0300

    [15.0][ADD] pricelist_cache_ux: extiende pricelist_cache (OCA)

---
+++ pricelist_cache_ux/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 juanpgarza - Juan Pablo Garza <juanp@juanpgarza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "pricelist_cache_ux",
+    "summary": "",
+    "version": "15.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/juanpgarza/sale-addons",
+    "author": "juanpgarza",
+    "license": "AGPL-3",
+    "depends": [
+                "sale",
+                "pricelist_cache",
+            ],
+    "data": [
+        'views/pricelist_cache_views.xml',
+        ],
+    "installable": True,
+}
